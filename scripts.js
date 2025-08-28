88d2d43837667cb122a88e6a72a09a1fbba45ad1
AbdElmoniem2003 <elmoniemabd0@gmail.com>
2025-08-28 21:51:19 +0300

    day 10

---
+++ scripts.js
@@ -0,0 +1,62 @@
+let ol = document.querySelector( 'ol' );
+let input = document.querySelector( 'input' );
+let btn = document.querySelector( '.add' );
+
+function ipEnter ()
+{
+    if ( !input.value.trim() ) return;
+    let li = document.createElement( 'li' );
+    li.innerHTML = `<span>${ input.value }  </span>  <button onclick=deleteLI(this)> Delete</button> <button onclick=toggleMark(this)> check</button>`;
+    ol.appendChild( li );
+    input.value = null
+}
+
+
+function deleteLI ( ele )
+{
+    ele.parentElement.parentElement.remove()
+}
+function toggleMark ( ele )
+{
+    ele.parentElement.querySelector( 'span' ).classList.toggle( 'mark' )
+}
+
+
+
+function checkSTD ( ele )
+{
+    ele.parentElement.parentElement.classList.toggle( 'check' )
+}
+
+let form = document.querySelector( 'form' );
+let tableBody = document.querySelector( 'tbody' );
+let id = document.querySelector( 'input#userID' );
+let userName = document.querySelector( 'input#userName' );
+let course = document.querySelector( 'input#course' );
+let payment = document.querySelector( 'input#payment' );
+let newStd = document.querySelector( '.newStd' );
+
+form.addEventListener('submit', (ev)=>
+
+{
+    event.preventDefault()
+    tableBody.innerHTML += `
+            <tr>
+                <td>${id.value}</td>
+                <td>${userName.value}</td>
+                <td>${course.value}</td>
+                <td>${payment.value}</td>
+                <td>
+                    <button class="del-btn" onclick="deleteLI(this)">delete</button>
+                    <button class="check-btn" onclick="checkSTD(this)">check </button>
+
+                </td>
+            </tr>
+
+`
+    id.value = null
+    userName.value = null
+    course.value = null
+    payment.value = null
+})
+
